65b88b8ba79d11afeb7ae0fa6ef4db2cabd08257
dzeno <dzenan.brcaninovic@ipi-akademija.ba>
2023-03-24 18:37:50 +0100

    Novi Activity sa fragment view, plus dvije klase ja

---
+++ app/src/main/java/com/hakaton/binaryhakaton/Artikal.java
@@ -0,0 +1,4 @@
+package com.hakaton.binaryhakaton;
+
+public class Artikal {
+}
---
+++ app/src/main/java/com/hakaton/binaryhakaton/ui/home/HomeFragment.java
@@ -0,0 +1,48 @@
+package com.hakaton.binaryhakaton.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.hakaton.binaryhakaton.LoginForm;
+import com.hakaton.binaryhakaton.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        binding.personButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), LoginForm.class);
+                startActivity(intent);
+            }
+        });
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}

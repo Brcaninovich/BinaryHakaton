7d784fad7423a476f4e32c1cdaf90f0ef10b2852
M1rzoni <mirza.pohotographycannon@gmail.com>
2023-03-25 00:26:11 +0100

    Loading Screen preko JSON fileova

---
+++ app/src/main/java/com/hakaton/binaryhakaton/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.hakaton.binaryhakaton;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    TextView pik;
+    LottieAnimationView lottie;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        pik = findViewById(R.id.pik);
+        lottie = findViewById(R.id.lottie);
+
+
+
+
+        //kritika.animate().translationY(-1400).setDuration(2700).setStartDelay(0);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i = new Intent(getApplicationContext(), GlavniMenu.class);
+                startActivity(i);
+                finish();
+
+            }
+        }, 3000);
+    }
+}

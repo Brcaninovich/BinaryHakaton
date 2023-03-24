88c09eb3940177b582d6b35d72d695c7bfd7cd6c
M1rzoni <mirza.pohotographycannon@gmail.com>
2023-03-24 21:46:41 +0100

    AssetFile

--- app/src/main/java/com/hakaton/binaryhakaton/LoginForm.java
+++ app/src/main/java/com/hakaton/binaryhakaton/LoginForm.java
@@ -2,13 +2,32 @@ package com.hakaton.binaryhakaton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class LoginForm extends AppCompatActivity {
+
+    TextView register_button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_form);
+        register_button= (TextView) findViewById(R.id.register_button);
+
+        register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //binding.loginLayout.setVisibility(View.GONE);//Cover u slucaju da radi na visible/hide
+                //binding.odabirLayout.setVisibility(View.VISIBLE);//Cover u slucaju da radi na visible/hide
+                Intent intent = new Intent(LoginForm.this,RegisterForm.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/hakaton/binaryhakaton/RegisterForm.java
@@ -0,0 +1,17 @@
+package com.hakaton.binaryhakaton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class RegisterForm extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_form);
+    }
+}

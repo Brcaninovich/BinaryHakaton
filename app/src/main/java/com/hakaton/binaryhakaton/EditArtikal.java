133299fb8b820a9a0f9f626a0050b2427b3fa21e
M1rzoni <mirza.pohotographycannon@gmail.com>
2023-03-25 10:13:32 +0100

    Dodavanje funkcije za editovanje artikala

---
+++ app/src/main/java/com/hakaton/binaryhakaton/EditArtikal.java
@@ -0,0 +1,14 @@
+package com.hakaton.binaryhakaton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditArtikal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_artikal);
+    }
+}
